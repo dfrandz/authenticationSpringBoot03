33285993a13803157b6a8757b3924873e65ee2ef
dfrandz <dfrandz8@gmail.com>
2023-04-04 00:03:57 +0200

    remove jwt key

--- security/src/main/java/com/frandz/security/model/User.java
+++ security/src/main/java/com/frandz/security/model/User.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 @Data
